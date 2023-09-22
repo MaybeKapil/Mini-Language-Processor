a5ac81c82b25f66a4f207870d93d114c452480ef
MaybeKapil <117200663+MaybeKapil@users.noreply.github.com>
2023-09-21 21:14:02 -0400

    Modified variable and function name to follow Python naming convention

--- fileReader.py
+++ fileReader.py
@@ -3,13 +3,13 @@ class fileReader:
     # Global Variables
 
     # Holds the current character
-    currentChar = ""
+    current_char = ''
 
     # Holds the current character's line number
-    currentLine = 0 
+    current_line = 0 
 
     # Holds the current character's column number
-    currentColumn = 0
+    current_column = 0
 
     # Reads the next character in the input file. Does not return anything.
     # Will cause the next character to be stored in memory.
@@ -19,13 +19,13 @@ class fileReader:
         currentColumn += 1
 
     # Returns a string containing the character that was just read
-    def current_char():
+    def get_current_char():
         return "fix_me"
 
     # Returns a string containing the line and column integers separated by a colon character.
     def position():
         return currentLine +  ":" + currentColumn;
-
+    
     def main():
         print("fix_me")
 
