6067a25858ebcef97ec4e77f50b5181740316a4b
MaybeKapil <117200663+MaybeKapil@users.noreply.github.com>
2023-09-22 13:29:28 -0400

    Implemented os.path for verifying if user inputted file exists. Added the function get_file_name() for getting a valid user input for file name.

--- FileReader.py
+++ FileReader.py
@@ -1,3 +1,5 @@
+# Import the 'isfile' function from the 'os.path' submodule.
+import os.path
 
 # Global Variables
 
@@ -10,8 +12,8 @@ current_line = 0
 # Holds the current character's column number
 current_column = 0
 
-class fileReader: 
-    
+class fileReader:
+
     # Reads the next character in the input file. Does not return anything.
     # Will cause the next character to be stored in memory.
     def next_char():
@@ -28,7 +30,25 @@ class fileReader:
     def position():
         pos  = current_line +  ":" + current_column
         return pos
-    
+
+    # Prompt the user to enter a valid file name.
+    # Returns the valid file name entered by the user.
+    def get_file_name():
+        while(True):
+            # Assume the file is in the same directory as the executable reader.
+            file_name = input("Enter the file name: ")
+            path = './' + file_name
+
+            # Verify if the user input corresponds to a valid file.
+            # If the input is invalid, prompt the user again for a valid file name until a valid file name is obtained.
+            if (os.path.isfile(path) == False):
+                print("Invalid file. Please enter a valid file name: ")
+                continue
+            else:
+                break
+
+        return file_name
+
     def main():
         print("fix_me")
 
