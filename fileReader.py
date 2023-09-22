98ea77489f0f068c93d588917d28e36eb580d80a
MaybeKapil <117200663+MaybeKapil@users.noreply.github.com>
2023-09-21 22:16:08 -0400

    Fixed global variable scope

--- fileReader.py
+++ fileReader.py
@@ -1,22 +1,24 @@
-class fileReader:
 
-    # Global Variables
+# Global Variables
 
-    # Holds the current character
-    current_char = ''
+# Holds the current character
+current_char = ''
 
-    # Holds the current character's line number
-    current_line = 0 
+# Holds the current character's line number
+current_line = 0
 
-    # Holds the current character's column number
-    current_column = 0
+# Holds the current character's column number
+current_column = 0
 
+class fileReader: 
+    
     # Reads the next character in the input file. Does not return anything.
     # Will cause the next character to be stored in memory.
     def next_char():
-        currentChar = 
-        currentLine += 1
-        currentColumn += 1
+        global current_char, current_line, current_column
+        current_char = 'a'
+        current_line += 1
+        current_column += 1
 
     # Returns a string containing the character that was just read
     def get_current_char():
